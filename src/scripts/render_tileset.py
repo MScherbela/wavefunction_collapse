eeefa1866689fc046b8aff98cd7f1bd72f878e13
Michael Scherbela <michael.scherbela@gmail.com>
2024-11-29 10:20:56 +0100

    fix path

--- src/scripts/render_tileset.py
+++ src/scripts/render_tileset.py
@@ -3,7 +3,7 @@ from wavefunction_collapse.tiles import load_tiles, render_tiles
 from wavefunction_collapse.generator import generate
 import numpy as np
 
-tiles, constraints, tile_weights = load_tiles("../../tiles/road")
+tiles, constraints, tile_weights = load_tiles("../../tiles/example")
 n_tiles = tiles.shape[0]
 
 map_shape = [20, 40]
@@ -13,9 +13,7 @@ tile_map = np.ones(map_shape, dtype=int) * n_tiles
 circle_radius = 5
 row = np.arange(map_shape[0])[:, None]
 col = np.arange(map_shape[1])[None, :]
-is_in_circle = (row - map_shape[0] / 2) ** 2 + (
-    col - map_shape[1] / 2
-) ** 2 < circle_radius**2
+is_in_circle = (row - map_shape[0] / 2) ** 2 + (col - map_shape[1] / 2) ** 2 < circle_radius**2
 tile_map[is_in_circle] = 15
 
 # and a triangle of grass in the top left
